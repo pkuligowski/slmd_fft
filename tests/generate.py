623dd03176858ad54e1554ffd460088508d7c49f
Piotr Kuligowski <xsaperx@gmail.com>
2020-06-15 01:01:00 +0200

    noised sinusoid generator, doc screenshots, dB in and out view

--- tests/generate.py
+++ tests/generate.py
@@ -1,14 +1,16 @@
 import math
 import matplotlib.pyplot as plt
+import random
 
-FREQ_HZ = 650
+FREQ_HZ = 50
 SAMPLING_FREQ = 4096
+NOISE_AMPLITUDE = 0.1
 
 output_str = []
 output = []
 imag = []
 for i in range(0, SAMPLING_FREQ):
-    value = math.sin(2 * math.pi * (i % (SAMPLING_FREQ / FREQ_HZ)) / (SAMPLING_FREQ / FREQ_HZ))
+    value = math.sin(2 * math.pi * (i % (SAMPLING_FREQ / FREQ_HZ)) / (SAMPLING_FREQ / FREQ_HZ)) + (random.random()-0.5) * 2 * NOISE_AMPLITUDE
     output.append(value)
     output_str.append(str(round(value, 3)))
     imag.append(str(0))
